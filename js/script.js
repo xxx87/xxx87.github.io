2280434fc3a5166c976934c39a455237beae31bf
Ilya Isaev <xxx_87@ukr.net>
2018-06-13 17:10:49 +0300

    add project

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@ var images = document.querySelectorAll('#gallery .photos img');
 var i = 0;
 
 btn_prev.onclick = function(){
-     images[i].style.display = 'none';
+     images[i].className = '';
      i = i - 1; /* i-- */
      
      if(i < 0){
          i = images.length - 1;
      }
      
-     images[i].style.display = 'block';
+     images[i].className = 'showed';
 }
 
 btn_next.onclick = function(){
-     images[i].style.display = 'none';
+     images[i].className = '';
      i = i + 1; /* i++ */
      
      if(i >= images.length){
          i = 0;
      }
      
-     images[i].style.display = 'block';
+     images[i].className = 'showed';
 }
